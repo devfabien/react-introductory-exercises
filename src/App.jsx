d9592e13dc2dc3737445a1945e69c94c8315c07f
devfabien <ish001fabi@gmail.com>
2023-09-29 14:48:28 +0200

    Introductory exercises refactoring codes for exercise3

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,15 @@
-import Button from "./components/exercise3/button"
+import Button from "./components/exercise3/button";
 
 function App() {
-
   return (
     <>
-    <div className=" p-5 flex justify-center gap-5">
-      <Button number={1}/>
-      <Button number={2}/>
-      <Button number={3}/>
-    </div>
+      <div className=" p-5 flex justify-center gap-5">
+        <Button number={1} />
+        <Button number={2} />
+        <Button number={3} />
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
