a3707d95ceb82a4f1bd73c9f687334cc72e5575c
devfabien <ish001fabi@gmail.com>
2023-09-29 14:54:33 +0200

    Introductory exercises refactoring codes for exercise6

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
-import AnimalList from "./components/exercise6/list"
-import data from '../src/components/exercise6/data.json'
+import AnimalList from "./components/exercise6/list";
+import data from "../src/components/exercise6/data.json";
+
 function App() {
-const animal=data.map((item,index)=><AnimalList id={index} name={item.name} image={item.url}/>)
+  const animal = data.map((item, index) => (
+    <AnimalList id={index} name={item.name} image={item.url} />
+  ));
   return (
     <>
-    <div>
-      {animal}
-    </div>
+      <div>{animal}</div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
